abe49dfec2d283fa970e2d2c376366b1cc635bf6
Amira Azeer <amira@linearsix.com>
2023-06-01 17:21:44 +0530

    Daily Chart Completed

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,9 @@ import Customers from "scenes/customers";
 import Transaction from "scenes/transaction";
 import Geography from "scenes/geography";
 import Sales from "scenes/overview";
+import Daily from "scenes/daily";
+
+
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -32,7 +35,8 @@ function App() {
               <Route path="/transactions" element={<Transaction/>} />
               <Route path="/geography" element={<Geography/>} />
               <Route path="/overview" element={<Sales/>} />
-
+              <Route path="/daily" element={<Daily/>} />
+              
             </Route>
           </Routes>
 
